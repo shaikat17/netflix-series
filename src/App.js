3a741b0cc21eaee9c490ba3ed724f5cec6fbbd45
shaikat <shaikatpal56@gmail.com>
2021-09-09 00:48:28 +0600

    Made some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-function App() {
-  return (
-    <p>jksdf</p>
+import React from 'react';
+import Card from './Card'
+import netflixData from './netflix'
+
+
+const App = () => (
+  <>
+  <h1 className="heading_style">List of 10 Series</h1>
+  <div className="cards">
+    {netflixData.map( val => {
+      return (
+    <Card 
+    key={val.id}
+    imgSrc={val.imageSrc}
+    title={val.title} 
+    link={val.link} />
+    )
+    })}
+    </div>
+    </>
   )
-}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,33 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Card from './Card'
-import netflixData from './netflix'
 
-// console.log(netflixData[0])
-
-// function iCard(val) {
-//   console.log(val)
-
-//   return (
-//     <Card imgSrc={val.imageSrc}
-//     title={val.title} 
-//     link={val.link} />
-//     )
-// }
 
 ReactDOM.render(
   <>
-  <h1 className="heading_style">List of 10 Series</h1>
-  <div className="cards">
-    {netflixData.map( val => {
-      return (
-    <Card imgSrc={val.imageSrc}
-    title={val.title} 
-    link={val.link} />
-    )
-    })}
-    </div>
+  <App />
     </>,
   document.getElementById('root')
 );
